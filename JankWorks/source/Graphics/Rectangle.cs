621693a1e9029e3a617a2cf0b9b3c5cc1bb90860
Ross <rossfuge@outlook.com>
2021-08-19 13:26:22 +0100

    Adds float rectangle

--- JankWorks/source/Graphics/Rectangle.cs
+++ JankWorks/source/Graphics/Rectangle.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 using System.Runtime.InteropServices;
 
 namespace JankWorks.Graphics
@@ -22,5 +23,26 @@ namespace JankWorks.Graphics
         public bool Equals(Rectangle other) => this == other;
         public static bool operator ==(Rectangle a, Rectangle b) => a.Position == b.Position && a.Size == b.Position;
         public static bool operator !=(Rectangle a, Rectangle b) => a.Position != b.Position || a.Size != b.Position;       
+    }
+
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Bounds : IEquatable<Bounds>
+    {
+        public Vector2 Position;
+        public Vector2 Size;
+
+        public Bounds(float left, float top, float width, float height) : this(new Vector2(left, top), new Vector2(width, height)) { }
+
+        public Bounds(Vector2 position, Vector2 size)
+        {
+            this.Position = position;
+            this.Size = size;
+        }
+        public override int GetHashCode() => this.Position.GetHashCode() ^ this.Size.GetHashCode();
+        public override bool Equals(object obj) => obj is Bounds other && this == other;
+        public bool Equals(Bounds other) => this == other;
+        public static bool operator ==(Bounds a, Bounds b) => a.Position == b.Position && a.Size == b.Position;
+        public static bool operator !=(Bounds a, Bounds b) => a.Position != b.Position || a.Size != b.Position;
     }
 }
