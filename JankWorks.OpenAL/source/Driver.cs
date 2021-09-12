2191843525fb1c89da16c46c962b05d1dbcb484d
Ross <rossfuge@outlook.com>
2021-09-12 20:42:04 +0100

    Adds OpenAL native functions

---
+++ JankWorks.OpenAL/source/Driver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using JankWorks.Audio;
+
+using JankWorks.Drivers;
+using JankWorks.Drivers.Audio;
+
+[assembly: JankWorksDriver(typeof(JankWorks.Drivers.OpenAL.Driver))]
+
+namespace JankWorks.Drivers.OpenAL
+{
+    public sealed class Driver : IAudioDriver
+    {
+        public AudioDevice GetDefaultAudioDevice()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
